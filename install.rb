b702612a65a6349286d9bc35699abf3bc8a8ed69
Ryan Stout <ryanstout@gmail.com>
2010-02-13 16:38:41 -0700

    works in irbZ

--- install.rb
+++ install.rb
@@ -4,5 +4,5 @@ puts "Testing install"
 require 'rails_generator'
 require 'rails_generator/scripts/generate'
 
-ARGV = ['snapmon']
-Rails::Generator::Scripts::Generate.new.run(['snapmon_config'])
+# ARGV = ['snapmon']
+# Rails::Generator::Scripts::Generate.new.run(['snapmon_config'])
