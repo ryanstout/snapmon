7763b63aa9b23ac47820a5ccb9aaff73893022e1
Ryan Stout <ryanstout@gmail.com>
2010-02-13 16:44:41 -0700

    Fixing

--- install.rb
+++ install.rb
@@ -4,5 +4,4 @@ puts "Testing install"
 require 'rails_generator'
 require 'rails_generator/scripts/generate'
 
-# ARGV = ['snapmon']
-# Rails::Generator::Scripts::Generate.new.run(['snapmon_config'])
+Rails::Generator::Scripts::Generate.new.run(['snapmon_config'])
